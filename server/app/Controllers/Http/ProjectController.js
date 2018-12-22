3d1d059fbc715bffa7540a512541b0eba1a9f681
Mouad <mouad.lousimi@etud.iga.ac.ma>
2018-12-22 15:48:14 -0500

    destroy with exception

---
+++ server/app/Controllers/Http/ProjectController.js
@@ -0,0 +1,38 @@
+'use strict'
+
+
+const Project = use('App/Models/Project');
+
+const AuthorizationService = use('App/Services/AuthorizationService');
+
+class ProjectController {
+
+     async  index( { auth } ) {
+          const user = await auth.getUser()
+          return await user.projects().fetch();
+     }
+
+    async create( { auth,request} ) {
+      const user = await auth.getUser();
+      const { title, description } = request.all();
+      const project = new Project();
+      project.fill({
+        title,
+        description
+      });
+      await user.projects().save(project);
+      return project;
+    }
+
+    async  destroy( { auth , request, params } ) {
+      const user = await auth.getUser()
+      const { id } = params
+      const project = await Project.find(id);
+      AuthorizationService.verifyPermission(project, user)
+      await project.delete();
+      return project;
+
+  }
+}
+
+module.exports = ProjectController
